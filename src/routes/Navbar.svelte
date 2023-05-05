cfd9f3b0bff52a4d11580aa6f5edeb07d45bfffb
iotah <writeforever@foxmail.com>
2023-05-05 15:52:31 +0800

    this might be okay
    Signed-off-by: iotah <writeforever@foxmail.com>

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,17 +1,15 @@
 <script>
-	import { each } from "svelte/internal";
+	import { each } from 'svelte/internal';
 
-    let buttons = [
-        { id: '1', name: "Home" },
-    ];
+	let buttons = [{ id: '1', name: 'Home' }];
 </script>
 
 <h1>Welcome to embeddedboys.org!</h1>
 
 <ul>
-{#each buttons as button}
-    <li>
-    {button.name}
-    </li>
-{/each}
+	{#each buttons as button}
+		<li>
+			{button.name}
+		</li>
+	{/each}
 </ul>

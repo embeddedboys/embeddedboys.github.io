882f3dec6c2acc20aa8f33529cb995c3f2d9c592
iotah <writeforever@foxmail.com>
2023-05-05 14:15:14 +0800

    initial commit
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { each } from "svelte/internal";
+
+    let buttons = [
+        { id: '1', name: "Home" },
+    ];
+</script>
+
+<h1>Welcome to embeddedboys.org!</h1>
+
+<ul>
+{#each buttons as button}
+    <li>
+    {button.name}
+    </li>
+{/each}
+</ul>
